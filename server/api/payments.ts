ad7f1c451e3f85a68f7edaad4f674352c12074ce
OnMaine <volodymyr.vasylenko@futurragroup.com>
2024-10-08 14:50:56 +0200

    fix/

--- server/api/payments.ts
+++ server/api/payments.ts
@@ -1,8 +1,9 @@
-import { readFileSync } from 'fs'
+import { defineEventHandler } from 'h3'
+import { promises as fs } from 'fs'
 import { resolve } from 'path'
 
-export default defineEventHandler(() => {
-  const filePath = resolve('./assets/payments.json')
-  const fileContents = readFileSync(filePath, 'utf-8')
-  return JSON.parse(fileContents)
+export default defineEventHandler(async () => {
+  const dataPath = resolve('server/data/payments.json')
+  const data = await fs.readFile(dataPath, 'utf-8')
+  return JSON.parse(data)
 })
--- server/api/skins.ts
+++ server/api/skins.ts
@@ -1,9 +1,11 @@
-import { readFileSync } from 'fs'
+import { defineEventHandler } from 'h3'
+import { promises as fs } from 'fs'
 import { resolve } from 'path'
 
-export default defineEventHandler(() => {
-  const filePath = resolve('./assets/skins.json')
-  const fileContents = readFileSync(filePath, 'utf-8')
-  const { data } = JSON.parse(fileContents)
+export default defineEventHandler(async () => {
+  const dataPath = resolve('server/data/skins.json')
+  const data1 = await fs.readFile(dataPath, 'utf-8')
+
+  const { data } = JSON.parse(data1)
   return data
 })
