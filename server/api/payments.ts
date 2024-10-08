0494bea56dbd3948606ab4a7f033165b2819fe5a
OnMaine <volodymyr.vasylenko@futurragroup.com>
2024-10-08 14:36:53 +0200

    fix/

--- server/api/payments.ts
+++ server/api/payments.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
 export default defineEventHandler(() => {
-  const filePath = resolve('assets/payments.json')
+  const filePath = resolve('./assets/payments.json')
   const fileContents = readFileSync(filePath, 'utf-8')
   return JSON.parse(fileContents)
 })
--- server/api/skins.ts
+++ server/api/skins.ts
@@ -2,7 +2,7 @@ import { readFileSync } from 'fs'
 import { resolve } from 'path'
 
 export default defineEventHandler(() => {
-  const filePath = resolve('assets/skins.json')
+  const filePath = resolve('./assets/skins.json')
   const fileContents = readFileSync(filePath, 'utf-8')
   const { data } = JSON.parse(fileContents)
   return data
